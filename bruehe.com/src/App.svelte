ec43c678ff278fb05cc17adad71957789fa2ac19
Florian Becker <florian00m.becker@gmail.com>
2023-06-19 11:30:59 +0000

    initial commit

---
+++ bruehe.com/src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import Bruehe from './lib/Bruehe.svelte';
+</script>
+
+<main>
+  <Bruehe />
+
+  <h1>DAS IST BRÜHE</h1>
+</main>
+
+<style>
+</style>
---
+++ bruehe.com/src/lib/Bruehe.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  import Bruehe from "../assets/bruehe.webp";
+</script>
+
+<img src={Bruehe} alt="Brühe">
